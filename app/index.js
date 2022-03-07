ec0cee10bfc9e4f47a99c7e6d3a485caacb7254a
niraj-kumar-r <nirajk23523@gmail.com>
2022-03-07 13:39:29 +0530

    made the onclick event for the questions

--- app/index.js
+++ app/index.js
@@ -1,1 +1,28 @@
-console.log("It works");
+questionNode = document.querySelectorAll(".faq-questions");
+//even empty space is treated as sibling, so always use nextElementSibling
+
+for (let childQuestion of questionNode) {
+    // childQuestion.mouseover = (event) => {
+    //     childQuestion.style.color = "var(--primary-highlight)";
+    // };
+    childQuestion.onclick = (event) => {
+        console.log(childQuestion.nextElementSibling.style.opacity, "opacity");
+        if (childQuestion.nextElementSibling.style.opacity == "1") {
+            childQuestion.nextElementSibling.style.opacity = "0";
+            childQuestion.nextElementSibling.style.height = "0px";
+            childQuestion.nextElementSibling.style.margin = "0";
+            childQuestion.style.fontWeight = "400";
+            childQuestion.style.color = "var(--neutral-bold)";
+
+            console.log("unclicked");
+        } else {
+            childQuestion.nextElementSibling.style.opacity = "1";
+            childQuestion.nextElementSibling.style.height = "auto";
+            childQuestion.nextElementSibling.style.margin = "1em auto 1em";
+            childQuestion.style.fontWeight = "700";
+            childQuestion.style.color = "var(--primary-normal)";
+
+            console.log("clicked");
+        }
+    };
+}
